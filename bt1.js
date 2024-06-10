ba8ab38fb9040cdddc28f85d424d7926672cb548
Admin <lmht721@gmail.com>
2024-06-10 09:22:51 +0700

    add bt1

---
+++ bt1.js
@@ -0,0 +1,12 @@
+let a = 10
+let b = 9.5
+let c = true
+let d = 'Ho Chi Minh City'
+
+document.write('Math = ' + a);
+document.write("</br>");
+document.write("Physics = " + b);
+document.write("</br>");
+document.write("c = " + c);
+document.write("</br>");
+document.write("The city: " + d)
