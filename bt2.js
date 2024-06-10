7f4af4ece1c37276c299a04fce4178de5b7bd6be
Admin <lmht721@gmail.com>
2024-06-10 09:36:06 +0700

    add bt2

---
+++ bt2.js
@@ -0,0 +1,11 @@
+var width = 10
+var height = 20
+var area = width * height
+var perimeter = (width + height)*2
+document.write("Width = " + width + "m")
+document.write("</br>")
+document.write("Height = " + height + "m")
+document.write("</br>")
+document.write("Perimeter = " + perimeter)
+document.write("</br>")
+document.write("Area = " + area)
